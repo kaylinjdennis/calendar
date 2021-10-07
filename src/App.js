0a4f37cf6c83223f8f60cbb7caed314581d645ec
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-10-06 23:12:31 -0600

    Add dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import React from "react";
+import WeekView from './components/WeekView';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={WeekView} />
+				</Switch>
+			</BrowserRouter>
+		</>
 	);
 }
 
